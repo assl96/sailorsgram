a8ddc4710ebb3a9f5e0abe4ccf34e3e6897df8cc
ana sofia <anasofiaserraluz@gmail.com>
2020-05-01 16:29:04 +0100

    agregado estilos

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -6,10 +6,21 @@
 </script>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
     :global(body) {
         background-color: rgb(255, 142, 179);
         color: rgb(41, 51, 182);
+        font-family: 'Lato', sans-serif;
+        margin: 0;
+        padding: 0;
     }
+
+    :global(h1,h2,h3){
+        margin: 0;
+        padding: 0;
+    }
+    
 </style>
 
 <Header />
